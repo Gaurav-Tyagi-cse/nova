629524d2cc840c3d7f6e3d9bed1edeee23dd6584
Gaurav Tyagi <118004334+Gaurav-Tyagi-cse@users.noreply.github.com>
2024-11-25 17:33:11 +0530

    first commit

---
+++ typing.js
@@ -0,0 +1,7 @@
+// var typed = new Typed('#dev1', {
+//     strings: ['WELCOME TO EDUFY NOVA'],
+//     typeSpeed: 80,
+//     smartBackspace: false,
+//     fadeOut: true,
+//     loop: false,
+//   });
